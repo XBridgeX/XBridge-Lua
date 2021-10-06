4dfcf2283fb54ca44b0d104d71f3842b51576d41
Lition <79248548+Lition802@users.noreply.github.com>
2021-10-06 22:25:39 +0800

    Create app.lua

---
+++ LUA/app.lua
@@ -0,0 +1,20 @@
+local json = require('./dkjson')
+local ws = GetWebsocketClient('ws://127.0.0.1:8080','生存服务器','password')
+k = ws.getK
+iv = ws.getiv
+ws:AddFunction('onMessage',function (s,m)
+	print('['..s..'] '..m)
+	local raw = json.decode(m).params.raw
+	print(raw)
+	print(AESDecrypt(raw,k,iv))
+end)
+ws:AddFunction('onOpen',function (s)
+	print('服务器['..s..'] 连接成功')
+end)
+ws:AddFunction('onClose',function (s)
+	print('服务器['..s..'] 断开连接')
+end)
+ws:AddFunction('onErrpr',function (s,m)
+	print('服务器['..s..'] 连接异常：'..m)
+end)
+ws:Start()
